c4fc920cac017bb6697dad870ab3f25224fa851f
ElenaKareva <9036142240lm@mail.ru>
2022-04-25 22:03:28 +0300

    added files

---
+++ src/main/java/CreditCalculator.java
@@ -0,0 +1,17 @@
+public class CreditCalculator {
+    public static void main(String[] args) {
+
+    }
+
+    public int sumPerMonth(int sum, double rate, int period) {
+        return 0;
+    }
+
+    public int fullSumReturn(int sum, double rate, int period) {
+        return 0;
+    }
+
+    public int overPaySum(int sum, double rate, int period) {
+        return 0;
+    }
+}
